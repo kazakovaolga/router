99099e03b61f2aaa0ebfbae81bad3e8eba1daf7d
kazakovaolga <kazakovaolga@users.noreply.github.com>
2022-09-08 06:16:49 +0000

    Deploying to gh-pages from @ kazakovaolga/router@4d1939f034cff2527067deea58918a74fd1ff941 🚀

--- images/index.ts
+++ images/index.ts
@@ -4,8 +4,7 @@ var router = Router();
 var header = document.querySelector('.header');
 var root = document.getElementById('root');
 var footer = document.getElementById('footer');
-router.on(/.*/, createLogger(header, "/.*"), createImage(root, "/.*"), leftPage(footer, "/.*")); //router.on(/.*/, createLogger(header), createImage(root), leftPage(footer,"/.*"));
-
+router.on(/\//, createLogger(header, ""), createImage(root, ""), leftPage(footer, ""));
 router.on(function (path) {
   return path === "/contacts";
 }, createLogger(header, "/contacts"), createImage(root, "/contacts"), // onEnter    
@@ -16,11 +15,6 @@ leftPage(footer, "/about"));
 router.on("/about/us", createLogger(header, "/about/us"), createImage(root, "/about/us"), // createImage(root,"/about/us"),
 leftPage(footer, "/about/us"));
 document.querySelector('body').addEventListener("click", function (event) {
-  console.log('event=', event); // const el=event.target;
-  // if (!el.matches("a")) {
-  //     return;
-  // }
-
   if (event !== null && event.target !== null) {
     var element = event.target;
 
@@ -30,11 +24,8 @@ document.querySelector('body').addEventListener("click", function (event) {
 
     ;
     var url = element.getAttribute("href");
-    console.log(url);
     router.go(url);
   }
 
-  event.preventDefault(); // let url = event.target.getAttribute("href");
-  // router.go(url);
-  //unsubscribe();
+  event.preventDefault();
 });
